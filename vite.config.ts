6d1961f641525206bbfb610037a16957bbabeb85
Ultra-Code <mega.alpha100@gmail.com>
2021-07-24 14:53:29 +0000

    Add iconify with vite support

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,17 @@ import vue from '@vitejs/plugin-vue'
 import pages from 'vite-plugin-pages'
 import layout from 'vite-plugin-vue-layouts'
 import components, { HeadlessUiResolver } from 'vite-plugin-components'
+import icons, { ViteIconsResolver } from 'vite-plugin-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), pages(), layout(), components({ customComponentResolvers: [HeadlessUiResolver()] })],
+    plugins: [vue(), pages(), layout(), icons(),
+    components({ customComponentResolvers: [HeadlessUiResolver(), ViteIconsResolver()] })],
     server: {
         fs: {
             // Allow serving files from one level up to the project root
             allow: ['..', '.', '/'],
-            strict:true,
+            strict: true,
         }
     }
 })
