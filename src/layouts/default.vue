d560c380a425f9bda0417e053674a6cb61c56175
Ultra-Code <mega.alpha100@gmail.com>
2021-07-25 08:53:54 +0000

    setup default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container mx-auto">
+        <header><Header/></header>
+        <main>
+            <router-view />
+        </main>
+        <footer></footer>
+    </div>
+</template>
