8b5d1c68fd7d1e68b28b5a7454f30bedaf043b1f
Ultra-Code <mega.alpha100@gmail.com>
2021-07-25 08:47:17 +0000

    Use routes in vue app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import "./assets/index.css"
+import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
 import generatedRoutes from 'virtual:generated-pages'
 
-createApp(App).mount('#app')
-const routes = setupLayouts(generatedRoutes);
-const route = createRouter({ history: createWebHistory(), routes })
+const pageRoutes = setupLayouts(generatedRoutes);
+const routes = createRouter({ history: createWebHistory(), routes: pageRoutes })
+createApp(App).use(routes).mount('#app')
