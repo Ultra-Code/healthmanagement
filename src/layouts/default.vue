c2d92571a4c5f5bc2a1d9041cce97c06287df074
Ultra-Code <mega.alpha100@gmail.com>
2021-07-26 22:15:38 +0000

    Format src files with prettier

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container mx-auto">
-        <header><Header/></header>
+        <header><Header /></header>
         <main>
             <router-view />
         </main>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { createApp } from 'vue'
+import { createApp } from "vue"
 import "./assets/index.css"
-import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import { setupLayouts } from 'virtual:generated-layouts'
-import generatedRoutes from 'virtual:generated-pages'
+import App from "./App.vue"
+import { createRouter, createWebHistory } from "vue-router"
+import { setupLayouts } from "virtual:generated-layouts"
+import generatedRoutes from "virtual:generated-pages"
 
-const pageRoutes = setupLayouts(generatedRoutes);
-const routes = createRouter({ history: createWebHistory(), routes: pageRoutes })
-createApp(App).use(routes).mount('#app')
+const pageRoutes = setupLayouts(generatedRoutes)
+const routes = createRouter({
+    history: createWebHistory(),
+    routes: pageRoutes,
+})
+createApp(App).use(routes).mount("#app")
