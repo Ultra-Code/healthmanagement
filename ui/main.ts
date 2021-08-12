cc7816dbd1e9f1b42913f1bbf1851a5e042ce1bd
Ultra-Code <mega.alpha100@gmail.com>
2021-08-12 21:09:51 +0000

    Use pinia in vue app

--- ui/main.ts
+++ ui/main.ts
@@ -4,10 +4,11 @@ import App from "./App.vue"
 import { createRouter, createWebHistory } from "vue-router"
 import { setupLayouts } from "virtual:generated-layouts"
 import generatedRoutes from "virtual:generated-pages"
+import { createPinia } from "pinia"
 
 const pageRoutes = setupLayouts(generatedRoutes)
 const routes = createRouter({
     history: createWebHistory(),
     routes: pageRoutes,
 })
-createApp(App).use(routes).mount("#app")
+createApp(App).use(routes).use(createPinia()).mount("#app")
