7395503cfc4c099c202b4160180dd3953ab0988d
Ultra-Code <mega.alpha100@gmail.com>
2021-08-12 21:11:50 +0000

    Add basic support for signin into the system

--- ui/pages/signin.vue
+++ ui/pages/signin.vue
@@ -1,8 +1,30 @@
 <template>
-<AuthSignInForm/>
+    <AuthSignInForm :loginFunction="loginPatient" />
 </template>
 
-<route lang='yaml'>
-    meta:
-        layout : plain
+<route>
+    {
+    meta:{
+    layout : "plain"
+    }
+    }
 </route>
+
+<script lang="ts" setup>
+    import {useLoginStore} from "../store/loginstore"
+
+    const loginstore = useLoginStore()
+
+    const loginPatient = () => {
+        const formData = new FormData();
+        /* data.set("email", loginstore.email)
+        data.set("password", loginstore.password) */
+        fetch("http://localhost:80/api/patients/login", {
+            method:"POST",
+            mode: "cors",
+            body: formData
+        }).then((ServerResponse) => {
+
+        }).catch((Errors) => {throw Errors.body})
+    }
+</script>
