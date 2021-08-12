cd1d026e88be6c2741c187794a8ad6589a212f07
Ultra-Code <mega.alpha100@gmail.com>
2021-08-12 19:06:51 +0000

    Add CORS handling code on server

--- server/main.cc
+++ server/main.cc
@@ -1,10 +1,24 @@
 #include <drogon/drogon.h>
-int main() {
-    //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0",80);
-    //Load config file
-    //drogon::app().loadConfigFile("../config.json");
-    //Run HTTP framework,the method will block in the internal event loop
-    drogon::app().run();
-    return 0;
+
+int
+main()
+{
+  // Set HTTP listener address and port
+  drogon::app().addListener("0.0.0.0", 80);
+  // Load config file
+  // drogon::app().loadConfigFile("../config.json");
+  // Run HTTP framework,the method will block in the internal event loop
+
+  drogon::app().registerPostHandlingAdvice(
+      [](const drogon::HttpRequestPtr &req,
+         const drogon::HttpResponsePtr &resp) {
+        // LOG_DEBUG << "postHandling1";
+        resp->addHeader("Access-Control-Allow-Origin", "*");
+        resp->addHeader("Access-Control-Allow-Methods", "OPTIONS,POST");
+        resp->addHeader("Access-Control-Allow-Headers",
+                        "x-requested-with,content-type");
+        resp->addHeader("Access-Control-Allow-Credentials", "true");
+      });
+  drogon::app().run();
+  return 0;
 }
