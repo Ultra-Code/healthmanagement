e20f943d97a09af66ba7a767fa85713852eb8196
Ultra-Code <mega.alpha100@gmail.com>
2021-08-13 18:32:17 +0000

    move login logic into login store

--- ui/pages/signin.vue
+++ ui/pages/signin.vue
@@ -1,5 +1,5 @@
 <template>
-    <AuthSignInForm :loginFunction="loginPatient" />
+    <AuthSignInForm/>
 </template>
 
 <route>
@@ -9,22 +9,3 @@
     }
     }
 </route>
-
-<script lang="ts" setup>
-    import {useLoginStore} from "../store/loginstore"
-
-    const loginstore = useLoginStore()
-
-    const loginPatient = () => {
-        const formData = new FormData();
-        /* data.set("email", loginstore.email)
-        data.set("password", loginstore.password) */
-        fetch("http://localhost:80/api/patients/login", {
-            method:"POST",
-            mode: "cors",
-            body: formData
-        }).then((ServerResponse) => {
-
-        }).catch((Errors) => {throw Errors.body})
-    }
-</script>
