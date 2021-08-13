de634f2a2b60d71f55e02987c2263cb449c09b86
Ultra-Code <mega.alpha100@gmail.com>
2021-08-13 18:33:48 +0000

    use login store in authsiginform

--- ui/components/forms/AuthSignInForm.vue
+++ ui/components/forms/AuthSignInForm.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
-    import {PropType} from 'vue'
-    const props = defineProps({patientLogin: {type: Function as PropType<() => void>}})
+    import {useLoginStore} from "../../store/loginstore"
+
+    const loginstore = useLoginStore()
+
 </script>
 
 <template>
@@ -59,11 +61,11 @@
                         </div>
                     </button>
                 </div>
-                <form class="mt-6" @submit.prevent="patientLogin">
+                <form class="mt-6" @submit.prevent="loginstore.loginPatient">
                     <div>
                         <label class="block text-xs font-medium leading-relaxed tracking-tighter textblueGray-700">Email
                             Address</label>
-                        <input type="email" name="" id="" placeholder="Your Email "
+                        <input v-model="loginstore.data.email" type="email" name="" id="" placeholder="Your Email "
                             class="w-full px-4 py-2 mt-2 text-base transition
                             duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-100 ext-black-700 focus:border-blueGray-500 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
                             style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;"
@@ -72,7 +74,7 @@
                     <div class="mt-4">
                         <label
                             class="block text-xs font-medium leading-relaxed tracking-tighter text-blueGray-700">Password</label>
-                        <input type="password" name="" id="" placeholder="Your Password" minlength="6"
+                        <input v-model="loginstore.data.password" type="password" name="" id="" placeholder="Your Password" minlength="6"
                             class="w-full px-4 py-2 text-base transition
                             duration-500 ease-in-out transform
                             border-transparent rounded-lg bg-gray-100 ext-black-700 focus:border-blueGray-500 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
