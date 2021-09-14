9405b3368dfb790029bf6d721ab763cd5a7e0639
Ultra-Code <mega.alpha100@gmail.com>
2021-09-14 19:31:59 +0000

    Move default to landing page
    landing page better discribes what that layout is for

---
+++ ui/layouts/landing-page.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <header class="max-w-full"><HeroSection /></header>
+        <main class="container mx-auto">
+            <router-view />
+        </main>
+        <footer class="max-w-full"><Footer/></footer>
+    </div>
+</template>
