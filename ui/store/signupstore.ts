508d8043b6a1c15376b0648c28db62037990b575
Ultra-Code <mega.alpha100@gmail.com>
2021-09-16 00:39:19 +0000

    Initail signupstore

---
+++ ui/store/signupstore.ts
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia'
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
+
+export const useSignUpStore = defineStore("signUpStore", () => {
+
+    const data = reactive({ username:"", email: "", password: "" })
+
+    const router = useRouter()
+
+    const signUpPatient = () => {
+        const formData = new FormData();
+        formData.set("username",data.username)
+        formData.set("email", data.email)
+        formData.set("password", data.password)
+        fetch("http://localhost:80/api/patients/signup", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+            },
+            body: formData
+        }).then((ServerResponse) => {
+            if (ServerResponse.status === 200) {
+                router.push({ name: 'appointment', path: '/appointment' })
+            }
+        }).catch((Errors) => { throw Errors })
+    }
+
+    return { data, signUpPatient }
+
+})
+
