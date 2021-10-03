6f1351bb9f348dc5e3e5dfffed58e2076586155c
Ultra-Code <mega.alpha100@gmail.com>
2021-10-03 17:29:36 +0000

    Format and Import image into ImageCard for pre-render

--- ui/components/card/ImageCard.vue
+++ ui/components/card/ImageCard.vue
@@ -1,45 +1,36 @@
+<script lang="ts" setup>
+    import SimilingDoctor from "../../assets/simling-doctor-on-world-health-day.webp"
+
+</script>
+
 <template>
     <!-- component -->
     <div>
         <div class="bg-white lg:py-12 lg:flex lg:justify-center">
             <div
-                class="
-                    bg-white
-                    lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg
+                class=" bg-white lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg
                 "
             >
                 <div class="lg:w-1/2">
                     <div
                         class="h-64 bg-cover lg:rounded-lg lg:h-full"
-                        style="
-                            background-image: url('https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&fit=crop&w=1351&q=80');
-                        "
+                        style="background-image: url('https://image.freepik.com/free-psd/world-health-day-banner-template_23-2148874901.jpg'); "
                     ></div>
                 </div>
                 <div class="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
                     <h2 class="text-3xl text-gray-800 font-bold">
-                        Build Your New
-                        <span class="text-indigo-600">Idea</span>
+                        World Health
+                        <span class="text-indigo-600">Day</span>
                     </h2>
                     <p class="mt-4 text-gray-600">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing
-                        elit. Quidem modi reprehenderit vitae exercitationem
-                        aliquid dolores ullam temporibus enim expedita aperiam
-                        mollitia iure consectetur dicta tenetur, porro
-                        consequuntur saepe accusantium consequatur.
+                    Stay safe and be healthy .  Wear a mask In general, you do not need to wear a mask in outdoor settings.
+                    In areas with high numbers of COVID-19 cases, consider wearing a mask in crowded outdoor settings and
+                    for activities with close contact with others who are not fully vaccinated.
                     </p>
                     <div class="mt-8">
                         <a
                             href="#"
-                            class="
-                                bg-gray-900
-                                text-gray-100
-                                px-5
-                                py-3
-                                font-semibold
-                                rounded
-                            "
-                            >Start Now</a
+                            class=" bg-gray-900 text-gray-100 px-5 py-3 font-semibold rounded " >Start Now</a
                         >
                     </div>
                 </div>
