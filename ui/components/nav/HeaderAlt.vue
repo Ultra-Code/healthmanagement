f66a48ac365bd2577f1ffad15a597bb71537c033
Ultra-Code <mega.alpha100@gmail.com>
2021-10-22 18:55:03 +0000

    Format nav components and Rename some buttons and make Header well spaced out

--- ui/components/nav/HeaderAlt.vue
+++ ui/components/nav/HeaderAlt.vue
@@ -2,33 +2,11 @@
     <Popover>
         <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
             <nav
-                class="
-                    relative
-                    flex
-                    items-center
-                    justify-between
-                    sm:h-10
-                    lg:justify-start
-                "
+                class="relative flex items-center justify-between sm:h-10 lg:justify-start"
                 aria-label="Global"
             >
-                <div
-                    class="
-                        flex
-                        items-center
-                        flex-grow flex-shrink-0
-                        lg:flex-grow-0
-                    "
-                >
-                    <div
-                        class="
-                            flex
-                            items-center
-                            justify-between
-                            w-full
-                            md:w-auto
-                        "
-                    >
+                <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                    <div class="flex items-center justify-between w-full md:w-auto">
                         <a href="#">
                             <span class="sr-only">Workflow</span>
                             <img
@@ -38,26 +16,10 @@
                         </a>
                         <div class="-mr-2 flex items-center md:hidden">
                             <PopoverButton
-                                class="
-                                    bg-white
-                                    rounded-md
-                                    p-2
-                                    inline-flex
-                                    items-center
-                                    justify-center
-                                    text-gray-400
-                                    hover:text-gray-500 hover:bg-gray-100
-                                    focus:outline-none
-                                    focus:ring-2
-                                    focus:ring-inset
-                                    focus:ring-indigo-500
-                                "
+                                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                             >
                                 <span class="sr-only">Open main menu</span>
-                                <i-heroicons-outline-menu
-                                    class="h-6 w-6"
-                                    aria-hidden="true"
-                                />
+                                <i-heroicons-outline-menu class="h-6 w-6" aria-hidden="true" />
                             </PopoverButton>
                         </div>
                     </div>
@@ -68,17 +30,8 @@
                         :key="item.name"
                         :href="item.href"
                         class="font-medium text-gray-500 hover:text-gray-900"
-                        >{{ item.name }}</a
-                    >
-                    <a
-                        href="#"
-                        class="
-                            font-medium
-                            text-indigo-600
-                            hover:text-indigo-500
-                        "
-                        >Log in</a
-                    >
+                    >{{ item.name }}</a>
+                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Log in</a>
                 </div>
             </nav>
         </div>
@@ -93,56 +46,25 @@
         >
             <PopoverPanel
                 focus
-                class="
-                    absolute
-                    top-0
-                    inset-x-0
-                    p-2
-                    transition
-                    transform
-                    origin-top-right
-                    md:hidden
-                "
+                class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
             >
                 <div
-                    class="
-                        rounded-lg
-                        shadow-md
-                        bg-white
-                        ring-1 ring-black ring-opacity-5
-                        overflow-hidden
-                    "
+                    class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden"
                 >
                     <div class="px-5 pt-4 flex items-center justify-between">
                         <div>
                             <img
                                 class="h-8 w-auto"
                                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                alt=""
+                                alt
                             />
                         </div>
                         <div class="-mr-2">
                             <PopoverButton
-                                class="
-                                    bg-white
-                                    rounded-md
-                                    p-2
-                                    inline-flex
-                                    items-center
-                                    justify-center
-                                    text-gray-400
-                                    hover:text-gray-500 hover:bg-gray-100
-                                    focus:outline-none
-                                    focus:ring-2
-                                    focus:ring-inset
-                                    focus:ring-indigo-500
-                                "
+                                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                             >
                                 <span class="sr-only">Close main menu</span>
-                                <i-heroicons-outline-x
-                                    class="h-6 w-6"
-                                    aria-hidden="true"
-                                />
+                                <i-heroicons-outline-x class="h-6 w-6" aria-hidden="true" />
                             </PopoverButton>
                         </div>
                     </div>
@@ -151,35 +73,13 @@
                             v-for="item in navigation"
                             :key="item.name"
                             :href="item.href"
-                            class="
-                                block
-                                px-3
-                                py-2
-                                rounded-md
-                                text-base
-                                font-medium
-                                text-gray-700
-                                hover:text-gray-900 hover:bg-gray-50
-                            "
-                            >{{ item.name }}</a
-                        >
+                            class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                        >{{ item.name }}</a>
                     </div>
                     <a
                         href="#"
-                        class="
-                            block
-                            w-full
-                            px-5
-                            py-3
-                            text-center
-                            font-medium
-                            text-indigo-600
-                            bg-gray-50
-                            hover:bg-gray-100
-                        "
-                    >
-                        Log in
-                    </a>
+                        class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                    >Log in</a>
                 </div>
             </PopoverPanel>
         </transition>
