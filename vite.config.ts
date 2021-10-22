8f83ff033362dadd611fa168373e4b76eb2e2ac6
Ultra-Code <mega.alpha100@gmail.com>
2021-10-22 13:16:57 +0000

    Use auto install from unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import IconsResolver from 'unplugin-icons/resolver'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), pages({ pagesDir: "ui/pages" }),
-    layout({ layoutsDir: "ui/layouts" }), Icons(),
+    layout({ layoutsDir: "ui/layouts" }), Icons({ autoInstall: true }),
     components({
         dirs: "ui/components",
         resolvers: [
